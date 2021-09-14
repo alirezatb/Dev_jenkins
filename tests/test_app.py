a34d41c1a2bedec52395ee78fddc47e0071cf3ab
alirezatb <ar.tabebordbar@gmail.com>
2021-09-14 17:22:47 +1000

    unit tests and Jenkins update 4

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,6 @@ from app import remove_no_pattern
 class Testapp(unittest.TestCase):
     def test_remove_no_pattern(self):
         get_value = remove_no_pattern()
-        self.assertEqual(get_value, 12)
+        self.assertEqual(get_value,12)
 
 
